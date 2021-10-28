3a984d116635a8db06d10612cd970c490380fc2e
vinhle000 <vinhle000@gmail.com>
2021-10-28 11:32:54 -0700

    removed unused dependencies

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 
 import React from "react";
 import { hot } from 'react-hot-loader/root';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import ReviewsAndRatings from './ReviewsAndRatings.js';
 
 class App extends React.Component {
