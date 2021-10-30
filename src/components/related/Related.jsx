252410a778f548b9355183afc699fef13e7e6dd6
Devon Li <devonli102@gmail.com>
2021-10-30 10:22:50 -0700

    added related component and Related.jsx file, imported in App.js

---
+++ src/components/related/Related.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Related = () => {
+  return (
+    <div>
+      Hello from Related
+    </div>
+  )
+}
+
+
+
+export default Related;
+// https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products?product_id = 40364/related
+
