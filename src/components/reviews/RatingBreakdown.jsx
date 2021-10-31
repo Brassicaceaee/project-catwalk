e8c47f512bb149b437e316771f2980e5d3003d8a
Mitch Rosenlund <merosenlund@gmail.com>
2021-10-30 20:09:30 -0700

    Added elements to RatingBreakdown

--- src/components/reviews/RatingBreakdown.jsx
+++ src/components/reviews/RatingBreakdown.jsx
@@ -3,7 +3,19 @@ import styles from "./reviews.module.css";
 
 const RatingBreakdown = () => {
   return (
-    <div className={styles.ratingBreakdown}>Rating Breakdown</div>
+    <div className={styles.ratingBreakdown}>
+      <h3>RATINGS & REVIEWS</h3>
+      <div>
+        <span>#.#</span>
+        <span>*****</span>
+      </div>
+      <div>5 Star *****</div>
+      <div>4 Star ****-</div>
+      <div>3 Star ***--</div>
+      <div>2 Star **---</div>
+      <div>1 Star *----</div>
+      <p>##% of reviews recommend this product</p>
+    </div>
   );
 }
 
