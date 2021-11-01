0388eae6ff29e2ad0edae715671b51986c454044
vinhle000 <vinhle000@gmail.com>
2021-11-01 14:36:28 -0700

    rename classes

--- src/components/overview/overview.jsx
+++ src/components/overview/overview.jsx
@@ -2,17 +2,24 @@ import React from 'react';
 
 
 const Overview = (props) => {
+  var overviewStyle = {
 
+      margin:  'auto 10%',
+      height: '950px',
+      border: '2px solid green',
+      display: 'grid',
+      gap: '10px',
+      // grid-template-columns: 'repeat(2, 1fr)',
+      // grid-auto-rows: 'minmax(100px, auto)',
+
+  }
   return(
-    <>
-    <div className='TopBar'>Logo</div>
-
-    <div className='SiteMessage'>Notification</div>
-
-    <div className='SelectedProduct'>ProductINFO</div>
-
-    <div className='BottomProductInfo'>Bottom</div>
-    </>
+    <div className='overview' style={overviewStyle}>
+      <div className='Image'>Image</div>
+      <div className='PrductInfo'>ProdcutInfo</div>
+      <div className='DescriptionOne'>ProductDescription</div>
+      <div className='DescriptionTwo'></div>
+    </div>
   )
 }
 
