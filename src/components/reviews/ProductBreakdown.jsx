fa0702a077ddb3cf20c0cf507344ed92a739dc61
Mitch Rosenlund <merosenlund@gmail.com>
2021-11-02 11:28:25 -0700

    Added fake data to work with

--- src/components/reviews/ProductBreakdown.jsx
+++ src/components/reviews/ProductBreakdown.jsx
@@ -7,4 +7,28 @@ const ProductBreakdown = () => {
   );
 }
 
+var product = [
+  {
+    label: 'Size',
+    score: 3.4,
+  },
+  {
+    label: 'Width',
+    score: 1,
+  },
+  {
+    label: 'Quality',
+    score: 4,
+  },
+]
+
+var subLabel = {
+  Size: ['Too small', 'Perfect', 'Too large'],
+  Width: ['Too narrow', 'Perfect', 'Too wide'],
+  Comfort: ['Uncomfortable', '', 'Perfect'],
+  Quality: ['Poor', '', 'Perfect'],
+  Length: ['Runs short', 'Perfect', 'Runs long'],
+  Fit: ['Runs tight', 'Perfect', 'Runs long']
+}
+
 export default ProductBreakdown;
