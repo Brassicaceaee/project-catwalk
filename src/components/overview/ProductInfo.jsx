8eaed88bd62f043f8e783440d8efbd3b42597188
vinhle000 <vinhle000@gmail.com>
2021-11-02 14:19:45 -0700

    Added flexbox to layout Product Info sub components

--- src/components/overview/ProductInfo.jsx
+++ src/components/overview/ProductInfo.jsx
@@ -5,8 +5,14 @@ import styles from './overview.module.css'
 const ProductInfo = (props) => {
 
   return(
-    <div>ProductInfo</div>
-
+    <div className={styles.infoComponent}>
+      <div className={styles.starReview}>starReview</div>
+      <div className={styles.category}>category</div>
+      <div className={styles.title}>title</div>
+      <div className={styles.price}>price</div>
+      <div className={styles.styles}>styles</div>
+      <div className={styles.addToCart}>addToCart</div>
+    </div>
   )
 
 
