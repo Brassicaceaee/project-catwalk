2c7280b3884e47c4453615e1973e234487eac595
Mitch Rosenlund <merosenlund@gmail.com>
2021-11-03 08:23:15 -0700

    Changed RatingBreakdown to dynamically render test data

--- src/components/reviews/RatingBreakdown.jsx
+++ src/components/reviews/RatingBreakdown.jsx
@@ -1,37 +1,83 @@
 import React from 'react'
-import Stars from "../Stars.jsx";
-import styles from "./reviews.module.css";
+import Stars from '../Stars.jsx';
+import styles from './reviews.module.css';
 
 const RatingBreakdown = () => {
+  var ratings = [
+    0,
+    7,
+    12,
+    20,
+    28,
+    106
+  ]
+  var total = ratings.reduce((sum, rating) => (sum + rating));
+  var weighted = ratings.reduce((sum, rating, index) => ((rating * index) + sum))
+  var average = Math.round((weighted / total) * 10) / 10;
+  var recommended = 132;
+
   return (
     <div className={styles.ratingBreakdown}>
       <h3>RATINGS & REVIEWS</h3>
       <div className={styles.ratingBar}>
-        <span className={styles.averageRating}>#.# </span>
-        <Stars rating={2.34}/>
+        <span className={styles.averageRating}>{average} </span>
+        <Stars rating={average}/>
       </div>
       {bars.map((bar, index) => {
-        let percent = index + '0' + '%';
+        let percent = (ratings[bar] / total * 100) + '%';
         return (
           <div key={index} className={styles.ratingBar}>
-            <span>{bar}</span>
+            <span>{bar} Star</span>
             <div className={styles.percentBar}>
               <div style={{ width: percent }}></div>
             </div>
           </div>
         );
       })}
-      <p>##% of reviews recommend this product</p>
+      <p>{Math.round(recommended / total * 100)}% of reviews recommend this product</p>
     </div>
   );
 }
 
+var reviewsMeta = {
+  "product_id": "40344",
+  "ratings": {
+      "1": "7",
+      "2": "12",
+      "3": "20",
+      "4": "28",
+      "5": "106"
+  },
+  "recommended": {
+      "false": "41",
+      "true": "132"
+  },
+  "characteristics": {
+      "Fit": {
+          "id": 135219,
+          "value": "2.5476190476190476"
+      },
+      "Length": {
+          "id": 135220,
+          "value": "2.5952380952380952"
+      },
+      "Comfort": {
+          "id": 135221,
+          "value": "2.8536585365853659"
+      },
+      "Quality": {
+          "id": 135222,
+          "value": "3.1111111111111111"
+      }
+  }
+}
+
 var bars = [
-  '5 Star',
-  '4 Star',
-  '3 Star',
-  '2 Star',
-  '1 Star'
+  5,
+  4,
+  3,
+  2,
+  1
 ]
 
 export default RatingBreakdown;
