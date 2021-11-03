4f69e532ecd72985296de8c3e8c7023b90c3b8b1
Yosef Groener <yosefgroener@gmail.com>
2021-11-03 17:36:50 -0500

    Added expand and collapse functionality

--- src/components/questions/AnswerList.jsx
+++ src/components/questions/AnswerList.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Answer from './Answer.jsx';
 import styles from './questions.module.css';
 
@@ -9,10 +9,36 @@ const AnswerList = (props) => {
   });
   let firstTwoAnswers = answers.slice(0, 2);
 
+  const [collapsed, toggleDisplayState] = useState(true);
+
+
+  const [displayed, setDisplayed] = useState(answers.slice(0, 2));
+  // useEffect(() => {
+  //   displayedAnswers = answers.slice(0, 2);
+  // })
+
+  const [expandCollapseText, changeButtonText] = useState('See More Answers');
+
+  const changeAnswerDisplay = () => {
+    if (collapsed) {
+      setDisplayed(answers);
+      changeButtonText('Collapse Answers');
+      toggleDisplayState(!collapsed);
+    } else {
+      setDisplayed(firstTwoAnswers);
+      changeButtonText('See More Answers');
+      toggleDisplayState(!collapsed);
+    }
+  }
+
+
   return(
-    <ul className={styles.answerList}>
-      {firstTwoAnswers}
-    </ul>
+    <div>
+      <ul className={styles.answerList}>
+        {displayed}
+      </ul>
+      <span onClick={changeAnswerDisplay}><b>{expandCollapseText}</b></span>
+    </div>
   )
 };
 
