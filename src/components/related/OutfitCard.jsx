c6d97154f6084eca2347522f9e225e34736bf319
Devon Li <devonli102@gmail.com>
2021-11-04 15:50:21 -0700

    added 2 components for outfit card feature, and linked them to the related component. Added a minimal css style.

---
+++ src/components/related/OutfitCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './related.module.css';
+
+const OutfitCard = () => {
+  return (
+    <div >
+      Outfit Card
+    </div>
+  )
+}
+
+
+export default OutfitCard;
