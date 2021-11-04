4e99fde9e4ff2439cfd8ab6b876931a71d0227bd
vinhle000 <vinhle000@gmail.com>
2021-11-03 20:14:13 -0700

    Added click event handler function. Currently changes style name to current style selected Thumbnail images are opaque when hovered over.

--- src/components/overview/StyleSelector.jsx
+++ src/components/overview/StyleSelector.jsx
@@ -1,24 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './overview.module.css';
-import json from './styleSample.json'
+import json from './styleSample.json';
+
 
 const StyleSelector = (props) => {
-  console.log(json)
-
   var data = json;
-  var styleOptions = data.results //all results of styles form ID
+  var styleOptions = data.results //all results of styles form ID, sample data
+  const [selectedStyle, setStyle] = useState(styleOptions[0]);
 
+
+  const handleClick = (event, option) => {
+
+    setStyle(option);
+    //TODO
+    // Change state of Image gallery if this option changes
+  }
 
   //For every style option, display the first photo thumbnail of the option
   return (
     <>
-      <p className={styles.styleText}>STYLE > <span>Selected Style</span></p>
+
+      <p className={styles.styleText}>STYLE > <span>{selectedStyle.name}</span></p>
 
       <div className={styles.styleSelection}>
 
       {styleOptions.map( (option) =>
-        <span className={styles.styleItem}>
-          <img className={styles.styleThumbnail} src={option.photos[0].thumbnail_url}/>
+        <span className={styles.styleItem} >
+          <img className={styles.styleThumbnail} src={option.photos[0].thumbnail_url} onClick={(e)=>{handleClick(e, option)}}/>
         </span>
       )}
       </div>
