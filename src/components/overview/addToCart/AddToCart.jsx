51dcafc9c575b519941b2efec41a3950f9fafde7
vinhle000 <vinhle000@gmail.com>
2021-11-05 10:55:13 -0700

    Changed size selector react element to use property of name instead of value to reflect the size on the page after selecting the size.

--- src/components/overview/addToCart/AddToCart.jsx
+++ src/components/overview/addToCart/AddToCart.jsx
@@ -8,11 +8,12 @@ const AddToCart = (props) => {
   var options = json.results;
   var currentSkus = options[0].skus; //Object of containing skus;
 
-  var skus = Object.values(currentSkus);
+  var skus = Object.values(currentSkus)
 
-  const [selectedSize, setSize] = useState('');
-  const [selectedSkuID, setSkuID] = useState('');
+  const [selectedSize, setSize] = useState('')
+  const [selectedSKU, setSKU] = useState('')
   const [selectedQuantity, setQuantity] = useState('');
+
   //TODO
   //Based on the selected size, get SKU, creat quantity selector
   //If no sizes, selector only has "OUT of Stock"
@@ -21,9 +22,28 @@ const AddToCart = (props) => {
   const handleSizeSelect = (event) => {
     var skuID = event.target.value
     var skuData = currentSkus[skuID]
-    setSkuID(skuID)
+
+    setSKU(skuID)
     setSize(skuData.size)
+
   }
+
+  let styleSelector;
+
+  if (Object.entries(currentSkus).length > 0){
+    styleSelector =
+             ( <select name={selectedSize} onChange={handleSizeSelect}>
+
+                {selectedSize === '' && <option>Select Style</option>}
+
+                {Object.entries(currentSkus).map( (sku) =>
+                <option value={sku[0]} key={sku[0]}> {sku[1].size} </option>)}
+
+              </select>);
+  } else {
+    styleSelector = <p>OUT OF STOCK</p>
+  }
+
 
 
   const handleQuantitySelect = (event) => {
@@ -31,43 +51,26 @@ const AddToCart = (props) => {
     setQuantity(event.target.value)
   }
 
-
-  let styleSelector;
-
-  if (Object.entries(currentSkus).length > 0){
-    styleSelector =
-    (<select value={selectedSize} onChange={handleSizeSelect}>
-                {selectedSize === '' && <option>Select Style</option>}
-                {Object.entries(currentSkus).map( (sku ) =>
-                <option value={sku[0]} key={sku[0]}> {sku[1].size} </option>)}
-              </select>);
-  } else {
-    styleSelector = <p>OUT OF STOCK</p>
-  }
-
-
   let quantitySelector;
 
-  if (selectedSkuID === '') {
-    // quantitySelector = <select><option> - </option></select>
-
+  if (selectedSKU === '') {
+    quantitySelector = <select><option> - </option></select>
   } else {
-    var num = currentSkus[selectedSkuID].quantity
+    var num = currentSkus[selectedSKU].quantity
+    //Generates array [0, 1, 2, 3, 4, ...] for quantity options,
+    //array is used for mapping out React option elements for the selector
     var quantityNumbers = Array.from(Array(num).keys())
-
 
     quantitySelector = (
               <select value={selectedQuantity} onChange={handleQuantitySelect}>
-                {quantityNumbers.map( (number) =>
+                {quantityNumbers.slice(1, quantityNumbers.length).map( (number) =>
                   <option value={number}>{number}</option>
                 )}
-                {/* <option>sjadkfhajks</option> */}
               </select>
-
     )
   }
 
-  //
+
   return (
       <>
       <div className={styles.addToCart}>
