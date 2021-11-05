598de94ea46b5545fe31d76652f6d7643cd60841
vinhle000 <vinhle000@gmail.com>
2021-11-05 15:06:18 -0700

    Added Image and styled to display in the flexbox

--- src/components/overview/ImageGallery.jsx
+++ src/components/overview/ImageGallery.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './overview.module.css'
+import json from './styleSample.json';
 
 
 const ImageGallery = (props) => {
 
-  var nums = Array.from(Array(7).keys())
-  console.log(nums)
+  var nums = Array.from(Array(7).keys()) //Used for testing flex box children size
+  // console.log(nums)
+
+  /*Gallery would need
+   -state of style index,
+    -style information,
+    chosen thumbnail index
+
+    */
+
+
+   var photos = json.results[0].photos;
+  // const [selectedStyle, setStyle] = useState() //Place holder for use of context to change style
+  const [mainPhoto, setMainPhoto] = useState(0);
+
+  console.log(photos)
+
   return(
 
     <div className={styles.galleryContainer}>
@@ -16,7 +32,9 @@ const ImageGallery = (props) => {
         <span className={styles.galleryThumbnailImages}> {num} </span>)}
 
       </span>
-      <span className={styles.galleryMain}>MainImage</span>
+      <span className={styles.galleryMain}>
+          <img className={styles.mainPhoto} src={photos[mainPhoto].url}/>
+      </span>
     </div>
 
   )
