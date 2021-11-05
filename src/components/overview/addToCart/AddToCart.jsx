612a7480c910114f0aadbc31e9fc511899696c3f
vinhle000 <vinhle000@gmail.com>
2021-11-05 11:15:10 -0700

    Refactoring, code clean up. Added max limit of displaying 15 for the Quantity selector

--- src/components/overview/addToCart/AddToCart.jsx
+++ src/components/overview/addToCart/AddToCart.jsx
@@ -12,7 +12,7 @@ const AddToCart = (props) => {
 
   const [selectedSize, setSize] = useState('')
   const [selectedSKU, setSKU] = useState('')
-  const [selectedQuantity, setQuantity] = useState('');
+  const [selectedQuantity, setQuantity] = useState(1);
 
   //TODO
   //Based on the selected size, get SKU, creat quantity selector
@@ -22,32 +22,28 @@ const AddToCart = (props) => {
   const handleSizeSelect = (event) => {
     var skuID = event.target.value
     var skuData = currentSkus[skuID]
-
     setSKU(skuID)
     setSize(skuData.size)
-
   }
 
   let styleSelector;
 
   if (Object.entries(currentSkus).length > 0){
-    styleSelector =
-             ( <select name={selectedSize} onChange={handleSizeSelect}>
+    styleSelector = (
+             <select name={selectedSize} onChange={handleSizeSelect}>
 
                 {selectedSize === '' && <option>Select Style</option>}
-
                 {Object.entries(currentSkus).map( (sku) =>
                 <option value={sku[0]} key={sku[0]}> {sku[1].size} </option>)}
 
-              </select>);
+              </select>
+    )
   } else {
     styleSelector = <p>OUT OF STOCK</p>
   }
 
 
-
   const handleQuantitySelect = (event) => {
-    console.log(event.target.value)
     setQuantity(event.target.value)
   }
 
@@ -57,15 +53,20 @@ const AddToCart = (props) => {
     quantitySelector = <select><option> - </option></select>
   } else {
     var num = currentSkus[selectedSKU].quantity
+    //Keeps quantity to only display 15 max
+    if (num > 15) {
+      num = 15
+    }
     //Generates array [0, 1, 2, 3, 4, ...] for quantity options,
     //array is used for mapping out React option elements for the selector
     var quantityNumbers = Array.from(Array(num).keys())
 
     quantitySelector = (
               <select value={selectedQuantity} onChange={handleQuantitySelect}>
+
                 {quantityNumbers.slice(1, quantityNumbers.length).map( (number) =>
-                  <option value={number}>{number}</option>
-                )}
+                  <option value={number} key={number}>{number}</option>)}
+
               </select>
     )
   }
