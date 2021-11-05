474884ac68e72b46bd9215cdd7b376724fbff35c
vinhle000 <vinhle000@gmail.com>
2021-11-04 21:33:58 -0700

    Fixed issue of size selector returning unexpected values. Now returns the SkuID, and SkuID is used to set Size

--- src/components/overview/addToCart/AddToCart.jsx
+++ src/components/overview/addToCart/AddToCart.jsx
@@ -8,50 +8,69 @@ const AddToCart = (props) => {
   var options = json.results;
   var currentSkus = options[0].skus; //Object of containing skus;
 
-  var skus = Object.values(currentSkus)
+  var skus = Object.values(currentSkus);
 
-  const [selectedSize, setSize] = useState('')
-  const [selectedSKU, setSKU] = useState('')
-
+  const [selectedSize, setSize] = useState('');
+  const [selectedSkuID, setSkuID] = useState('');
+  const [selectedQuantity, setQuantity] = useState('');
   //TODO
   //Based on the selected size, get SKU, creat quantity selector
   //If no sizes, selector only has "OUT of Stock"
   //If size not selected, render "-" for quantity selector
 
   const handleSizeSelect = (event) => {
-    var skuID = event.target.value[0]
-    var skuData = event.target.value[1]
-
-    setSKU(skuID)
+    var skuID = event.target.value
+    var skuData = currentSkus[skuID]
+    setSkuID(skuID)
     setSize(skuData.size)
   }
 
-  //TODO: Needs test
 
-  //Style Selector
   let styleSelector;
+
   if (Object.entries(currentSkus).length > 0){
-
     styleSelector =
-             ( <select value={selectedSize} onChange={handleSizeSelect}>
-
+    (<select value={selectedSize} onChange={handleSizeSelect}>
                 {selectedSize === '' && <option>Select Style</option>}
-
                 {Object.entries(currentSkus).map( (sku ) =>
-                <option value={sku} key={sku[0]}> {sku[1].size} </option>)}
-
+                <option value={sku[0]} key={sku[0]}> {sku[1].size} </option>)}
               </select>);
   } else {
     styleSelector = <p>OUT OF STOCK</p>
   }
 
+
+  let quantitySelector;
+
+  if (selectedSkuID === '') {
+    quantitySelector = <select><option> - </option></select>
+
+  } else {
+    var num = currentSkus[selectedSkuID].quantity
+    debugger
+    var quantityNumbers = Array.from(Array(num).keys())
+
+    quantitySelector = (
+              <select value={selectedQuantity} onChange={(e) => (console.log(e.targe.value))}>
+                {quantityNumbers.map( (num) => {
+                  <option value={num}>{num}</option>
+                })}
+
+              </select>
+
+    )
+  }
+
+  //
   return (
       <>
       <div className={styles.addToCart}>
         <span className={styles.size}>
           {styleSelector}
         </span>
-        <span className={styles.quantity}> quantity</span>
+        <span className={styles.quantity}>
+          {quantitySelector}
+        </span>
         <span className={styles.cartButton}>add To Cart</span>
         <span className={styles.cartStar}>star</span>
       </div>
