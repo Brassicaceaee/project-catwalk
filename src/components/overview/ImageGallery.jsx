a08db514bdce71ea4c8116e4da8a68b0ac139ca0
vinhle000 <vinhle000@gmail.com>
2021-11-05 13:47:36 -0700

    Added flexbox to Thumbnails list and created child spans for images.

--- src/components/overview/ImageGallery.jsx
+++ src/components/overview/ImageGallery.jsx
@@ -4,11 +4,18 @@ import styles from './overview.module.css'
 
 const ImageGallery = (props) => {
 
+  var nums = Array.from(Array(7).keys())
+  console.log(nums)
   return(
 
     <div className={styles.galleryContainer}>
 
-      <span className={styles.galleryThumbnails}>Thumbnails</span>
+      <span className={styles.galleryThumbnails}>
+
+        {nums.map((num) =>
+        <span className={styles.galleryThumbnailImages}> {num} </span>)}
+
+      </span>
       <span className={styles.galleryMain}>MainImage</span>
     </div>
 
