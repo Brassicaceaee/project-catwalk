3a6a81a1fd00e8902d8c34df28d2f24a1f9c8b8f
Yosef Groener <yosefgroener@gmail.com>
2021-11-06 17:28:41 -0500

    Answer form displaying but haven't been able to limit number of files to upload

--- src/components/questions/FormAnswer.jsx
+++ src/components/questions/FormAnswer.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import styles from './questions.module.css';
 
 const AnswerForm = (props) => {
 
+  const [uploadArray, updateArray] = useState(0);
+  let imageInput = document.getElementById('image-uploads');
+  // let images = imageInput.files;
+  // var loadFile = function(event) {
+	// var image = document.getElementById('output');
+	// image.src = URL.createObjectURL(event.target.files[0]);
+  // };
 
+  const ref = useRef(null);
 
-  var loadFile = function(event) {
-	var image = document.getElementById('output');
-	image.src = URL.createObjectURL(event.target.files[0]);
-  };
+// useEffect(() => {
+//   updateArray(ref.current);
+// })
+
 
   if (props.show) {
     return (
@@ -27,7 +35,7 @@ const AnswerForm = (props) => {
              <input type='text' placeholder='Example: jack@email.com' maxlength='60'></input>
             <span className={styles.inputSubtext}> “For authentication reasons, you will not be emailed”</span>
             <label> Upload your photos </label>
-            <input type='file' accept="image/*" multiple></input>
+            <input type='file' id='image-uploads' accept="image/*" ref={ref} multiple onChange={(e) => updateArray(imageInput.files) }></input>
             <div className={styles.actions}>
               <button onClick={props.modalButtonClick}> Close </button>
             </div>
