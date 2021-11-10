5b4578610a9f17a7919a39584ce1929723e268cb
Devon Li <89163211+DevonL1010@users.noreply.github.com>
2021-11-09 23:56:59 -0800

    Update SearchBar.jsx

--- src/components/questions/SearchBar.jsx
+++ src/components/questions/SearchBar.jsx
@@ -4,7 +4,7 @@ import styles from './questions.module.css';
 const SearchBar = (props) => {
 
   return(
-    <div id='search' className={styles.searchBar}>
+    <div className={styles.searchBar}>
       <form>
         <input type='text' placeholder='Have a question? Search for answers…'></input>
       </form>
