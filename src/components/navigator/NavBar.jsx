e4e52140772afd05c6c75a24d31acdeb7aaf3375
Mitch Rosenlund <merosenlund@gmail.com>
2021-11-11 22:17:12 -0800

    Getting carried away

--- src/components/navigator/NavBar.jsx
+++ src/components/navigator/NavBar.jsx
@@ -10,7 +10,9 @@ const NavBar = () => {
         <h1>Brassicaceae</h1>
         <div className='navbar-right'>
           <a href='#related'>Related</a>
+          <div className='divider'></div>
           <a href='#search'>Q & A</a>
+          <div className='divider'></div>
           <a href='#reviewsWidget'>Review</a>
         </div>
       </div>
